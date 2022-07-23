bad12911d66e8890580a8856c288f0059d4daaaf
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-07-23 21:13:59 +0300

    Update 41.js

--- js/41.js
+++ js/41.js
@@ -9,15 +9,19 @@ const atTheOldToad = {
     return this.potions;
   },
 
-  addPotion(newPotion) {
+
+
+  addPotion(newPotion = {}) {
     for (let potion of this.potions) {
-      if (potion.name !== newPotion.name) {
-        this.potions.push(newPotion);
-        return this.potions;
+      if (potion.name === newPotion.name) {
+        return `Error! Potion ${newPotion.name} is already in your inventory!`;
       }
-      return `Error! Potion ${newPotion.name} is already in your inventory!`;
-    }    
+        }  
+    this.potions.push(newPotion);
+      return this.potions;
   },
+
+
 
   removePotion(potionName) {
     for (let potion of this.potions) {
@@ -29,21 +33,32 @@ const atTheOldToad = {
     return `Potion ${potionName} is not in inventory!`;
   },
 
-    updatePotionName(oldName, newName) {
-    const potionIndex = this.potions.indexOf(oldName);
 
-    if (potionIndex === -1) {
-      return `Potion ${oldName} is not in inventory!`;
+  updatePotionName(oldName, newName) {
+    for (let potion of this.potions) {
+      //   const potionIndex = potion.name.indexOf(oldName);
+      // console.log(potionIndex);
+      if (potion.name === oldName) {
+        potion.name = newName;
+        return this.potions;
+        
+      }
     }
-
-    this.potions.splice(potionIndex, 1, newName);
+    
+    return `Potion ${oldName} is not in inventory!`;
+    // this.potions.splice(potionIndex, 1, newName);
   },
 
 };
 
 // console.log(atTheOldToad.getPotions());
 // console.log(atTheOldToad.addPotion({ name: "Invisibility", price: 620 }));
+// console.log(atTheOldToad.addPotion({ name: "Power potion", price: 270 }));
+// console.log(atTheOldToad.addPotion({ name: "Dragon breath", price: 700 }));
+
 // console.log(atTheOldToad.removePotion("Dragon breath"));
+// console.log(atTheOldToad.removePotion("Speed potion"));
+
+
 console.log(atTheOldToad.updatePotionName("Dragon breath", "Polymorth"));
-console.log();
-console.log();
+console.log(atTheOldToad.updatePotionName("Stone skin", "Invulnerability potion"));
